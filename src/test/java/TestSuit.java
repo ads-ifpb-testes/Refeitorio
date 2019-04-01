9c6ed3144ee6d0982c5a91aefdfbb7d545f1f587
Hexcz <hex_cz@hotmail.com>
2019-04-01 10:01:36 -0300

    Criação da Suite de testes

--- src/test/java/TestSuit.java
+++ src/test/java/TestSuit.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        BuscarAlunoTest.class,
+        BuscarVisitanteTest.class,
+        CriarAlunoTest.class,
+        CriarVisitanteTest.class,
+        RemoverAlunoTest.class
+})
 
 public class TestSuit {
+
 }
